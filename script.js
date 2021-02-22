004766ce2f8180f545d480f2bc414f41dcb04d10
Aviral Gupta <aviralgupta6@gmail.com>
2021-02-22 20:01:53 +0530

    adding-values-current-user

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 'use strict';
 
-const player1 = {
-  name: document.getElementById('name--0').textContent,
-  score: (document.getElementById('score--0').textContent = 0),
-  current: (document.getElementById('current--0').textContent = 0),
-};
+const name1 = document.getElementById('name--0').textContent;
+let score1 = Number(document.getElementById('score--0').textContent);
+let current1 = Number(document.getElementById('current--0').textContent);
 
-const player2 = {
-  name: document.getElementById('name--1').textContent,
-  score: (document.getElementById('score--1').textContent = 0),
-  current: (document.getElementById('current--1').textContent = 0),
-};
+const name2 = document.getElementById('name--1').textContent;
+let score2 = Number(document.getElementById('score--1').textContent);
+let current2 = Number(document.getElementById('current--1').textContent);
 
 // console.table(player1);
 // console.table(player2);
@@ -19,15 +15,23 @@ const player2 = {
 //   player1.score = 10;
 // })();
 
-// document.getElementById('score--0').textContent = player1.score;
 // console.log(player1.score);
 
-// document.querySelector('.dice').src = `dice-${diceNumber}.png`;
+const diceRoll = function () {
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+  document.querySelector('.dice').src = `dice-${diceNumber}.png`;
+  if (diceNumber !== 1) {
+    console.log('ok');
+    current1 += diceNumber;
+    console.log(current1);
+    document.getElementById('current--0').textContent = current1;
+    // = player1.score;
+  }
+};
+
+const playerSwitch = function () {};
 
 const rollDice = document.getElementById('roll-dice');
-rollDice.addEventListener('click', function () {
-  let diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log('click', diceNumber);
-  document.querySelector('.dice').src = `dice-${diceNumber}.png`;
-});
-// console.log(rollDice);
+rollDice.addEventListener('click', diceRoll);
+// console.log(diceNumer);
