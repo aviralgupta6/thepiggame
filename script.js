6c137d35d2702bb76af8f03ec754a8416e3a8a6f
Aviral Gupta <aviralgupta6@gmail.com>
2021-02-23 12:27:22 +0530

    V3-bugfixes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const rollDice = document.getElementById('roll-dice');
 const holdScore = document.getElementById('hold-dice');
 const newGame = document.getElementById('new-game');
+const playerActive0 = document.querySelector('.player--0');
+const playerActive1 = document.querySelector('.player--1');
 
 let player = false;
 
@@ -34,23 +36,42 @@ const diceRoll = function () {
     score1 = Number(document.getElementById('current--1').textContent);
   } else {
     player = !player;
+    current1 = 0;
+    current0 = 0;
+    if (!playerActive0.classList.contains('player--active')) {
+      playerActive1.classList.remove('player--active');
+      playerActive0.classList.add('player--active');
+    } else {
+      playerActive0.classList.remove('player--active');
+      playerActive1.classList.add('player--active');
+    }
   }
 };
 
 const finalScore = function () {
   console.log('Holding');
+  if (!playerActive0.classList.contains('player--active')) {
+    playerActive1.classList.remove('player--active');
+    playerActive0.classList.add('player--active');
+  } else {
+    playerActive0.classList.remove('player--active');
+    playerActive1.classList.add('player--active');
+  }
   if (player === false) {
     current1 = 0;
+    current0 = 0;
     document.getElementById('score--0').textContent =
       Number(document.getElementById('score--0').textContent) + score0;
-    document.getElementById('current--0').textContent = 0;
+    score1 = 0;
+
     document.getElementById('current--1').textContent = 0;
   }
   if (player === true) {
     current0 = 0;
+    current1 = 0;
     document.getElementById('score--1').textContent =
       Number(document.getElementById('score--1').textContent) + score1;
-    document.getElementById('current--1').textContent = 0;
+    score0 = 0;
     document.getElementById('current--0').textContent = 0;
   }
 
@@ -67,6 +88,11 @@ const reset = function () {
   document.getElementById('current--1').textContent = 0;
   current0 = 0;
   current1 = 0;
+  player = false;
+  if (!playerActive0.classList.contains('player--active')) {
+    playerActive1.classList.remove('player--active');
+    playerActive0.classList.add('player--active');
+  }
 };
 
 rollDice.addEventListener('click', diceRoll);
